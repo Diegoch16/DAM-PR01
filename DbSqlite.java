b3dbf399af8588c3400158544c40500ea18b63e9
Castelao <diegochenlo1@gmail.com>
2019-10-18 13:14:28 +0200

    DbSqlite - Crea la base de datos; si no ya esxiste

--- DbSqlite.java
+++ DbSqlite.java
@@ -1,16 +1,61 @@
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException; 
 
-
 public class DbSqlite {
+	
+	final String path = "./db/";
+	final String dbname = "miTienda.db";
 
-	public static void main(String[] args) {
-		String path = "./db/";
+	private static DbSqlite instance;
+	private DbSqlite(){
+		this.init();
+	}
+	public static DbSqlite getInstance(){
+		if(instance == null){
+			instance = new DbSqlite();
+		}
+		return instance;
+	}
+	
+	private void checkDbFiles() {
+		boolean fileExists = false;
+		System.out.println("Comprobando Base de Datos");
+		File PATH = new File(path);
+		if(!PATH.exists()) {
+			PATH.mkdirs();
+		}
+		File DB = new File(PATH,dbname);
+		if(DB.exists()) {
+			System.out.println("Base de Datos ya Creada");
+			this.init();
+			return;
+			
+		}
+		try {
+			System.out.println("Creando base de datos");
+			if(DB.createNewFile()) {
+				fileExists = true;
+	
+			}
+		}catch(IOException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}finally {
+			if(fileExists) {
+				System.out.println("Base de Datos, Creada");
+				this.init();
+			}
+		}
+	}
+	
+	public void init() {
 		Connection conn = null;
         try {
             //db parameters
-            String url = "jdbc:sqlite:"+path+"miTienda.db";
+            String url = "jdbc:sqlite:"+path+dbname;
             // create a connection to the database
             conn = DriverManager.getConnection(url);
             
@@ -18,6 +63,7 @@ public class DbSqlite {
             
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+            this.checkDbFiles();
         } finally {
             try {
                 if (conn != null) {
@@ -27,7 +73,11 @@ public class DbSqlite {
                 System.out.println(ex.getMessage());
             }
         }
-		
 	}
 
+	public static void main(String[] args) {
+		  DbSqlite db = DbSqlite.getInstance();
+		
+	}
+	
 }
